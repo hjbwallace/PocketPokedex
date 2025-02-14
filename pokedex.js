60664f58caf44c5584475bfa04f4ee9252af251a
Harrison Wallace <hjbwallace@gmail.com>
2025-02-14 20:51:22 +0800

    Loading cards from json file

--- pokedex.js
+++ pokedex.js
@@ -1,5 +1,82 @@
 function initialise() {
-  configureCards();
+  loadCards();
+}
+
+async function loadCards() {
+  try {
+    const response = await fetch('cardDatabase.json');
+    const data = await response.json();
+    displayCards(data);
+    configureCards();
+  } catch (error) {
+    console.error('Error loading card data:', error);
+  }
+}
+
+const rarityMap = {
+  d1: '◆',
+  d2: '◆◆',
+  d3: '◆◆◆',
+  d4: '◆◆◆◆'
+};
+
+const typeMap = {
+  0: 'Colorless',
+  1: 'Grass',
+  2: 'Fire',
+  3: 'Water',
+  4: 'Lightning',
+  5: 'Fighting',
+  6: 'Psychic',
+  7: 'Darkness',
+  8: 'Metal',
+  9: 'Dragon',
+  10: 'Supporter',
+  11: 'Item',
+  12: 'Tool'
+}
+
+function displayCards(sets) {
+  const setsElement = document.querySelector('#sets');
+
+  sets.forEach((set) => {
+    const setElement = document.createElement('div');
+    setElement.classList.add('set');
+    setsElement.appendChild(setElement);
+
+    const setHeading = document.createElement('h3');
+    setHeading.textContent = set.name;
+    setElement.appendChild(setHeading);
+
+    const setCardsElement = document.createElement('div');
+    setCardsElement.classList.add('set-cards');
+    setElement.appendChild(setCardsElement);
+
+    set.cards.forEach((card) => {
+      const cardElement = document.createElement('button');
+      cardElement.classList.add('card');
+      cardElement.classList.add(`card-${typeMap[card.type].toLowerCase()}`);
+      cardElement.classList.add(`card-missing`);
+
+      const nameElement = document.createElement('span');
+      nameElement.classList.add('name');
+      nameElement.textContent = card.name;
+
+      const idElement = document.createElement('span');
+      idElement.classList.add('id');
+      idElement.textContent = `#${card.number.toString().padStart(3, '0')} ${rarityMap[card.rarity]}`;
+
+      const countElement = document.createElement('span');
+      countElement.classList.add('count');
+      countElement.textContent = '0';
+
+      cardElement.appendChild(nameElement);
+      cardElement.appendChild(idElement);
+      cardElement.appendChild(countElement);
+
+      setCardsElement.appendChild(cardElement);
+    });
+  });
 }
 
 function configureCards() {
